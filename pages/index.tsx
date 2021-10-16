08a66ff09f622366b4ec5f48caa08831ed270532
Michael Markell <mmarkell@users.noreply.github.com>
2021-10-16 19:29:00 -0400

    CTA for new users

--- pages/index.tsx
+++ pages/index.tsx
@@ -91,16 +91,7 @@ function Home() {
             marginTop: '2rem',
           }}
         >
-          Verify your NFTs across all your social media {'  '}
-          <span
-            style={{
-              cursor: 'pointer',
-              color: 'black',
-            }}
-            onClick={goToDemo}
-          >
-            (or see a demo)
-          </span>
+          Verify your NFTs across all your social media
         </div>
         {!unsure && !needsOnboarding && (
           <div
@@ -144,6 +135,29 @@ function Home() {
         >
           {unsure ? '' : needsOnboarding ? 'sign up' : 'edit your profile'}
         </div>
+        {needsOnboarding ||
+          (unsure && (
+            <div
+              style={{
+                backgroundColor: '#1F4893',
+                border: 'none',
+                fontFamily: 'Space Grotesk',
+                fontSize: '1.4rem',
+                padding: '60px 20px',
+                transition: 'all .3s ease-in-out',
+                borderRadius: 10,
+                color: 'white',
+                width: '30%',
+                height: '30%',
+                cursor: 'pointer',
+                margin: 'auto',
+                marginTop: '2rem',
+              }}
+              onClick={goToDemo}
+            >
+              See a demo!
+            </div>
+          ))}
       </main>
 
       <style jsx>{`
