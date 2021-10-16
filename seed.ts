978e9a60eeec35226003014d45f77075676febbd
Michael Markell <mmarkell@users.noreply.github.com>
2021-10-16 09:59:20 -0400

    removing seed

--- seed.ts
+++ seed.ts
@@ -1,27 +1,26 @@
 import { prisma as db } from './db';
 
 async function main() {
-  const user = await db.user.create({
-    data: {
-      id: 1,
-      alias: 'michael',
-      address: '0x1c10eD2ffe6b4228005EbAe5Aa1a9c790D275A52',
-    },
-  });
-
-  const nft = await db.nft.create({
-    data: {
-      imageUrl:
-        'https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnVubnklMjBjYXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
-      address: '0xd539a3a5edb713e6587e559a9d007ffff92bd9ab',
-      type: 'ERC20',
-      User: {
-        connect: {
-          id: user.id,
-        },
-      },
-    },
-  });
+  // const user = await db.user.create({
+  //   data: {
+  //     id: 1,
+  //     alias: 'michael',
+  //     address: '0x1c10eD2ffe6b4228005EbAe5Aa1a9c790D275A52',
+  //   },
+  // });
+  // const nft = await db.nft.create({
+  //   data: {
+  //     imageUrl:
+  //       'https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnVubnklMjBjYXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
+  //     address: '0xd539a3a5edb713e6587e559a9d007ffff92bd9ab',
+  //     type: 'ERC20',
+  //     user: {
+  //       connect: {
+  //         id: user.id,
+  //       },
+  //     },
+  //   },
+  // });
 }
 
 main()
